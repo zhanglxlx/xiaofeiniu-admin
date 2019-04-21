724b29494686e1e2e77aae629c011d1bb97c5c98
zhanglxlx <zhanglx@tedu.cn>
2019-04-21 09:39:04 +0800

    登录功能

--- src/main.js
+++ src/main.js
@@ -10,6 +10,55 @@ import axios from 'axios'
 Vue.use(ElementUI)//注册ElementUI 插件
 Vue.prototype.$axios=axios; //把axiso设置为所有的Vue组件实例的成员属性，以后可以使用this.$axios使用一部请求功能
 Vue.config.productionTip = false
+// 创建全局过滤器
+Vue.filter("date",(val)=>{
+  //把bigint转换为YYYY-MM-DD
+  var date=new Date(val);
+  var yy=date.getFullYear();
+  var mm=date.getMonth()+1;
+  mm=mm>9?mm:"0"+mm;
+  var dd=date.getDate();
+  dd=dd>9?dd:"0"+dd;
+
+  var hh=date.getHours();
+  hh=hh>9?hh:"0"+hh;
+  var mi=date.getMinutes();
+  mi=mm>9?mi:"0"+mi;
+  var ss=date.getSeconds();
+  ss=ss>9?ss:"0"+ss;
+
+  return yy+"-"+mm+"-"+dd;
+
+})
+Vue.filter("datetime",(val)=>{
+  //把bigint转换为YYYY-MM-DD hh:mm:ss
+  var date=new Date(val);
+  var yy=date.getFullYear();
+  var mm=date.getMonth()+1;
+  mm=mm>9?mm:"0"+mm;
+  var dd=date.getDate();
+  dd=dd>9?dd:"0"+dd;
+
+  var hh=date.getHours();
+  hh=hh>9?hh:"0"+hh;
+  var mi=date.getMinutes();
+  mi=mm>9?mi:"0"+mi;
+  var ss=date.getSeconds();
+  ss=ss>9?ss:"0"+ss;
+
+  return yy+"-"+mm+"-"+dd+" "+hh+":"+dd+":"+ss;
+})
+Vue.filter("currency",(val)=>{
+  //把bigint转换为￥ xx.yy
+  return '￥'+val.toFixed(2);
+})
+Vue.filter('tableStatus',(val)=>{
+  if(val==1) return '空闲';
+  else if(val==2) return '预定';
+  else if(val==3) return '占用';
+  else return '其他';
+})
+
 
 new Vue({
   router,
